c6044eff6b06df3a1c2d2f723a48ad6d4e3f4e1b
Barnes <1293142201@qq.com>
2024-08-29 18:30:47 +0800

    新增部分菜品

--- app/src/main/java/com/example/whattoeat/MainActivity.java
+++ app/src/main/java/com/example/whattoeat/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 import java.io.IOException;
 import java.net.SocketTimeoutException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,30 +20,46 @@ public class MainActivity extends AppCompatActivity {
     private Button whatToEatButton;
     
     private TextView whatToEatText;
+//    private TextView easterEgg;
 
-    public String[] whatToEatList = {"瓦块烧","漂汤","铁锅炖大鹅","荤豆花","老麻抄手", "刀削面", "兰州拉面", "一食堂", "二食堂", "三食堂", "四食堂", "飘香兔", "寿喜锅", "甜品自助"};
+    public String[] whatToEatList = {"瓦块烧","漂汤","铁锅炖大鹅","荤豆花","老麻抄手", "刀削面", "兰州拉面", "飘香兔", "寿喜锅", "甜品自助", "土鸡米线", "鸡公煲", "干锅",
+                                     "干锅家烤鱼", "西门烤鱼", "冒菜", "王肥肠", "啵啵鱼", "霜霜美蛙鱼", "肥兔公社", "堡！！！"};
 
+    public Calendar calendar = Calendar.getInstance();
     public Random random = new Random();
+
+    public int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         whatToEatButton = findViewById(R.id.WhatToEatButton);
         whatToEatText = findViewById(R.id.WhatToEat);
+//        easterEgg = findViewById(R.id.easterEgg);
 
         View.OnClickListener clickListener = new ButtonClickListener();
 
         whatToEatButton.setOnClickListener(clickListener);
 
-//        String[] whatToEatList = {"瓦块烧","漂汤","铁锅炖大鹅","荤豆花","老麻抄手", "刀削面", "兰州拉面", "一食堂", "二食堂", "三食堂", "四食堂", "飘香兔", "寿喜锅", "甜品自助"};
+
     }
 
     private class ButtonClickListener implements View.OnClickListener {
         @Override
         public void onClick(final View v) {
             int randomNumber = random.nextInt(whatToEatList.length);
-
             whatToEatText.setText(whatToEatList[randomNumber]);
+
+//            if(dayOfWeek == 4) {
+//                String easterEggText = "KFC Crazy Thursday!V me 50 RMB!!";
+//                if(randomNumber == 4){
+//                    whatToEatText.setText(easterEggText);
+//                } else {
+//                    whatToEatText.setText(whatToEatList[randomNumber]);
+//                }
+//            } else {
+//                whatToEatText.setText(whatToEatList[randomNumber]);
+//            }
         }
     }
 }
